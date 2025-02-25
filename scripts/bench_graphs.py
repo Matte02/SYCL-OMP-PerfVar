b5c395ab0b0bde9656dbcf56a5e71e80d18fa070
Matte02 <mathias.pretot@gmail.com>
2025-02-25 11:15:44 +0100

    Update bench_graphs script to use new print format for duration

--- scripts/bench_graphs.py
+++ scripts/bench_graphs.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 import locale
+import re
 
 #Usage: Arg 1 contains the path to the folder where folders of benchmark outputs are stored
 def main():
@@ -38,17 +39,24 @@ def main():
             break
 
         exectimes = []
-
+        duration_re = re.compile(r"Total Duration: (\d+\.\d+) seconds")
         for file in files:
-            with open(benchpath+"/"+file, "r") as files:
-                first = files.readline()
-                for last in files: pass
-
-            exectimes.append(float(locale.delocalize(last[0:(len(last)-1)])))
+            
+            with open(benchpath+"/"+file, "r") as lines:
+                for line in lines:
+                    # Match and extract the total duration
+                    dur_match = duration_re.match(line)
+                    if dur_match:
+                        total_duration = dur_match.group(1)
+                        total_duration = float(total_duration)  # Convert to nanoseconds
+                        print(total_duration)
+                        exectimes.append(total_duration)
+                    
             
         exectimes.sort()
         print(len(exectimes))
         avgexectime = sum(exectimes)/len(exectimes)
+        print(avgexectime)
         normexectimes = list(map(lambda x: x/avgexectime, exectimes))
 
 
