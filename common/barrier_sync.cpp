4f86361aa6c785a59eca7de4d4b18c1336886d07
Matte <mathias.pretot@gmail.com>
2025-03-20 16:39:25 +0100

    Update barrier to not use busy wait

--- common/barrier_sync.cpp
+++ common/barrier_sync.cpp
@@ -8,37 +8,41 @@
 #include <time.h>
 #include <signal.h>
 
-const char* SEM_READY = "/sem_ready";
-const char* SEM_START = "/sem_start";
+const char *SEM_READY = "/sem_ready";
+const char *SEM_START = "/sem_start";
 
-sem_t* ready = nullptr;
-sem_t* start = nullptr;
+sem_t *ready = nullptr;
+sem_t *start = nullptr;
 
 #ifdef BARRIER_TIMEOUT
 #define TIMEOUT_SECONDS 10
 bool passed_barrier = false;
 timer_t timeoutid; // Make this global
-static void timeout_handler(int sig, siginfo_t *si, void *uc) {
+static void timeout_handler(int sig, siginfo_t *si, void *uc)
+{
     auto caught_signal = sig;
     std::cerr << "Timeout occurred: Process did not reach the barrier in time." << std::endl;
-    if (!passed_barrier) {
+    if (!passed_barrier)
+    {
         cleanup_semaphores();
         exit(EXIT_FAILURE);
-    }  
+    }
 }
 #endif
 
-void init_semaphores() {
+void init_semaphores()
+{
     // Initialize semaphores with total_processes
     ready = sem_open(SEM_READY, O_CREAT, 0644, 0); // Initial value: 0
     start = sem_open(SEM_START, O_CREAT, 0644, 0); // Initial value: 0
 
-    if (ready == SEM_FAILED || start == SEM_FAILED) {
+    if (ready == SEM_FAILED || start == SEM_FAILED)
+    {
         perror("sem_open");
         exit(EXIT_FAILURE);
     }
 
-    #ifdef BARRIER_TIMEOUT
+#ifdef BARRIER_TIMEOUT
     timer_t timeoutid;
     struct sigevent sevt;
     struct itimerspec itst;
@@ -48,7 +52,8 @@ void init_semaphores() {
     sat.sa_flags = SA_SIGINFO;
     sat.sa_sigaction = timeout_handler;
     sigemptyset(&sat.sa_mask);
-    if (sigaction(SIGRTMAX, &sat, NULL) == -1) {
+    if (sigaction(SIGRTMAX, &sat, NULL) == -1)
+    {
         perror("sigaction");
         exit(EXIT_FAILURE);
     }
@@ -57,7 +62,8 @@ void init_semaphores() {
     sevt.sigev_notify = SIGEV_SIGNAL;
     sevt.sigev_signo = SIGRTMAX;
     sevt.sigev_value.sival_ptr = &timeoutid;
-    if (timer_create(CLOCK_MONOTONIC, &sevt, &timeoutid) == -1) {
+    if (timer_create(CLOCK_MONOTONIC, &sevt, &timeoutid) == -1)
+    {
         perror("timeout_create");
         exit(EXIT_FAILURE);
     }
@@ -66,42 +72,49 @@ void init_semaphores() {
     itst.it_value.tv_nsec = 0;
     itst.it_interval.tv_sec = itst.it_value.tv_sec;
     itst.it_interval.tv_nsec = itst.it_value.tv_nsec;
-    if (timer_settime(timeoutid, 0, &itst, NULL) == -1) {
+    if (timer_settime(timeoutid, 0, &itst, NULL) == -1)
+    {
         perror("timer_settime");
         exit(EXIT_FAILURE);
     }
-    #endif
+#endif
 }
 
-void wait_for_barrier(int total_processes) {
+void wait_for_barrier(int total_processes)
+{
     // Signal that this process is ready
     sem_post(ready);
 
-    // Wait until all processes are ready
     int ready_count;
-    do {
-        sem_getvalue(ready, &ready_count);
-    } while (ready_count < total_processes);
+    sem_getvalue(ready, &ready_count);
 
-    // Signal start only once all processes are ready
-    sem_post(start);
+    // Last process to arrive releases all waiting processes
+    if (ready_count == total_processes)
+    {
+        for (int i = 0; i < total_processes; i++)
+        {
+            sem_post(start);
+        }
+    }
 
-    // Ensure all processes wait for the start signal
+    // Wait until the barrier is lifted
     sem_wait(start);
 
-    #ifdef BARRIER_TIMEOUT
+#ifdef BARRIER_TIMEOUT
     passed_barrier = true;
-    if (timer_delete(timeoutid) == -1) {
+    if (timer_delete(timeoutid) == -1)
+    {
         perror("timer_delete");
         exit(EXIT_FAILURE);
     }
-    #endif
+#endif
 }
 
-void cleanup_semaphores() {
+void cleanup_semaphores()
+{
     sem_close(ready);
     sem_close(start);
-    
+
     // Unlink semaphores when done
     sem_unlink(SEM_READY);
     sem_unlink(SEM_START);
