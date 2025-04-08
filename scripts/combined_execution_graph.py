f8042acd5d2f7bdf8ef10691def1887ad4b210ea
Matte <mathias.pretot@gmail.com>
2025-04-08 11:17:34 +0200

    Add coloring for smt labels

--- scripts/combined_execution_graph.py
+++ scripts/combined_execution_graph.py
@@ -58,18 +58,35 @@ def main():
 
     #plt.title(os.path.basename(input_folder))
     fig, ax = plt.subplots()
-    #plt.grid(axis = 'y', linestyle = '--')
     keys = exec_dict.keys()
     values = exec_dict.values()
     s = sorted(zip(keys, values), key = lambda y: tuple(reversed(y[0].split("-"))))
-    keys = [i[0] for i in s]
+
+    # Change "-NSMT-" to something else if needed
+    keys = [i[0].replace("-NSMT-", "-") for i in s]
     values = [i[1] for i in s]
 
     ax.set_title(os.path.basename(input_folder))
     ax.boxplot(values)
     ax.set_xticklabels(keys, fontsize=10)
+    ax.grid(True, which="major", axis="x", linewidth=0.5, linestyle='--')
+    ax.grid(True, which="major", axis="y", linewidth=0.5, linestyle='-')
     fig.autofmt_xdate()   
-    
+    fig.tight_layout(pad=2)
+
+    for i, (tick, orig_label) in enumerate(zip(ax.xaxis.get_major_ticks(), keys)):
+        
+        # Replace "-SMT-" with any other string to change the color
+        if "-SMT-" in orig_label:
+            color = 'royalblue'
+                        # Tick label and mark color
+            tick.label1.set_color(color)
+            tick.tick1line.set_color(color)
+            tick.tick2line.set_color(color)
+            tick.gridline.set_color(color)
+
+
+
     plt.savefig(os.path.join(output_folder, f"combined_boxplot.png"), dpi=250)
 
 if __name__ == "__main__":
